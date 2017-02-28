db4bb3c228107b39b0d3ba733c87968a86e5e64c
LTtravel <liting_2016@sina.com>
2017-02-28 22:40:16 +0800

    sort
    straight insertion sort

---
+++ SIS_1.js
@@ -0,0 +1,32 @@
+function insertSort(arr){
+	for ( i = 2; i <= arr.length; i++) {
+		if(arr[i] < arr[i-1]){
+			arr[0] = arr[i];
+			arr[i] = arr[i-1];
+			for(j = i-2; arr[0] < arr[j]; --j){
+				arr[j+1] = arr[j];
+			}
+			arr[j+1] = arr[0];
+		}
+	}
+}
+
+
+
+var arr = new Array;
+arr[1] = 2;
+arr[3] = 4;
+arr[2] = 8;
+arr[4] = 1;
+arr[5] = 3;
+arr[0] = null;
+insertSort(arr);
+console.log(arr);
+// for(var i=0;i<arr.length;i++){
+// 	console.log(arr[i]);
+// }
+// console.log(arr.length);
+
+// var ele = [1,3,5,9,7];
+// console.log(ele);
+// console.log(typeof ele);
