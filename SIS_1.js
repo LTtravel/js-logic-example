db60bbaf874507bedde832681373151d896b563c
LTtravel <liting_2016@sina.com>
2017-03-02 19:37:47 +0800

    update arr[0]
    修改数组输出的问题

--- SIS_1.js
+++ SIS_1.js
@@ -1,12 +1,14 @@
 function insertSort(arr){
-	for ( i = 2; i <= arr.length; i++) {
+	for (var i = 1; i <= arr.length; i++) {
 		if(arr[i] < arr[i-1]){
-			arr[0] = arr[i];
-			arr[i] = arr[i-1];
-			for(j = i-2; arr[0] < arr[j]; --j){
+			var sign;  //定义一个监视哨sign
+			sign = arr[i];  //将待插入的值arr[i]暂存到监视哨
+			arr[i] = arr[i-1];  //arr[i-1]向后移
+			for(var j = i-1; sign < arr[j]; --j){
+				//向后移动直到找到插入位置
 				arr[j+1] = arr[j];
 			}
-			arr[j+1] = arr[0];
+			arr[j+1] = sign;  // 找到插入位置后插入
 		}
 	}
 }
@@ -19,14 +21,10 @@ arr[3] = 4;
 arr[2] = 8;
 arr[4] = 1;
 arr[5] = 3;
-arr[0] = null;
+arr[0] = 0;
 insertSort(arr);
 console.log(arr);
-// for(var i=0;i<arr.length;i++){
-// 	console.log(arr[i]);
-// }
-// console.log(arr.length);
 
-// var ele = [1,3,5,9,7];
-// console.log(ele);
-// console.log(typeof ele);
+var arr2 = [23,45,33,11,36,19];
+insertSort(arr2);
+console.log(arr2);
