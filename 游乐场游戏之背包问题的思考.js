28af6d53cbea783f44963bf2009d8b341563c885
LTtravel <1359781852@qq.com>
2017-10-22 16:43:59 +0800

    define the class of thing
    定义物品类

---
+++ 游乐场游戏之背包问题的思考.js
@@ -0,0 +1,12 @@
+function createThing(name,weight,value){
+	var oThing = new Object;
+	oThing.name = name;
+	oThing.weight = weight;
+	oThing.value = value;
+	return oThing;
+}
+function take(bag,th){
+	//背包总容量bag，各个物品容量及价值存在数组thing中，使背包中物品价值最高
+	
+
+}
